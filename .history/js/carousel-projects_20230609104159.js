85182640bd79c43ad692b1253e0e90a0e262991a
runeunhjem <runeunhjem@gmail.com>
2023-06-09 10:42:39 +0200

    add sliding to projects

--- .history/js/carousel-projects_20230609104159.js
+++ .history/js/carousel-projects_20230609104159.js
@@ -1,38 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.querySelector(".carousel");
-  const prevButton = document.querySelector("#back-button");
-  const nextButton = document.querySelector("#next-button");
-  const carouselItems = Array.from(document.querySelectorAll(".carousel-item"));
 
-  let currentIndex = 0;
-
-  function showSlide(index) {
-    carousel.style.transform = `translateX(-${index * 100}%)`;
-    updateCounter(index);
-  }
-
-  function goToPrevSlide() {
-    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-    showSlide(currentIndex);
-  }
-
-  function goToNextSlide() {
-    currentIndex = (currentIndex + 1) % carouselItems.length;
-    showSlide(currentIndex);
-  }
-
-  function updateCounter(index) {
-    const counter = document.querySelector(".count");
-    counter.textContent = `${index + 1}/${carouselItems.length}`;
-  }
-
-  // Add event listeners for sliding functionality
-  carousel.addEventListener("swiperight", goToPrevSlide);
-  carousel.addEventListener("swipeleft", goToNextSlide);
-
-  prevButton.addEventListener("click", goToPrevSlide);
-  nextButton.addEventListener("click", goToNextSlide);
-});
 // document.addEventListener("DOMContentLoaded", function () {
 //   const carousel = document.querySelector(".carousel");
 //   const prevButton = document.querySelector("#back-button");
